e54f93bfe93e688aa2018cdb49690d64345bb7d2
ChessMaster1303 <57945166+ChessMaster1303@users.noreply.github.com>
2024-09-19 16:43:34 +0800

    Actual story game

--- dead_end_finder.py
+++ dead_end_finder.py
@@ -1,31 +1,36 @@
-import json
-story_data = json.load(open('story_data.json'))
-
-#Opening story
-print(story_data["plot"])
-
-#Find every scene that is referenced
-referenced_story_scene_keys = []
-for scene in story_data["scenes"]:
-    choices = story_data["scenes"][scene]["choices"]
-    for choice in choices:
-       if (not choice["scene_key"] in referenced_story_scene_keys):
-            referenced_story_scene_keys.append(choice["scene_key"])
-print("All referenced story keys: " + str(referenced_story_scene_keys))
-
-#Find every scene that is defined properly
-valid_story_scene_keys = []
-for scene in story_data["scenes"]:
-    valid_story_scene_keys.append(scene)
-print("All valid story keys: " + str(valid_story_scene_keys))
-
-#Find the dead ends (scenes that are referenced but not defined properly)
-dead_ends = []
-for scene in referenced_story_scene_keys:
-    if(not scene in valid_story_scene_keys):
-        dead_ends.append(scene)
-print("Dead-ends: " + str(dead_ends))
+def find_dead_ends():
+    import json
+    story_data = json.load(open('story_data.json'))
 
 
-#Keeps the command line open until user wishes to close it
-input("--------------------------------------\nPress 'Enter' to Exit")
+    #Find every scene that is referenced
+    referenced_story_scene_keys = []
+    for scene in story_data["scenes"]:
+        choices = story_data["scenes"][scene]["choices"]
+        for choice in choices:
+            if (not choice["scene_key"] in referenced_story_scene_keys):
+                referenced_story_scene_keys.append(choice["scene_key"])
+    #print("All referenced story keys: " + str(referenced_story_scene_keys))
+
+
+    #Find every scene that is defined properly
+    valid_story_scene_keys = []
+    for scene in story_data["scenes"]:
+        valid_story_scene_keys.append(scene)
+    #print("All valid story keys: " + str(valid_story_scene_keys))
+
+
+    #Find the dead ends (scenes that are referenced but not defined properly)
+    dead_ends = []
+    for scene in referenced_story_scene_keys:
+        if(not scene in valid_story_scene_keys):
+            dead_ends.append(scene)
+    return dead_ends
+    #print("Dead-ends: " + str(dead_ends))
+
+
+    #Keeps the command line open until user wishes to close it
+    #input("--------------------------------------\nPress 'Enter' to Exit")
+
+
+
---
+++ storyteller.py
@@ -0,0 +1,45 @@
+#Know the dead ends in the story, and hence know when to call ChatGPT
+from dead_end_finder import find_dead_ends
+dead_ends = find_dead_ends()
+
+#Opening story with the plot
+import json
+story_data = json.load(open('story_data.json'))
+print(story_data["plot"])
+
+
+def checkValidChoice(userChoice, choiceCounter):
+    if(type(userChoice) is int):
+        if(userChoice <= choiceCounter + 1):
+            return True
+    else:
+        return False
+
+
+def newScenePrint(scene_description, choices):
+    print(scene_description)
+    choiceCounter = 0
+    for choice in choices:
+        print(str(choiceCounter + 1) + ": " + choice["text"])
+
+    userChoice = int(input("What option do you choose? *USER INPUT: "))
+    validity = checkValidChoice(userChoice, choiceCounter)
+
+    if(validity == True):
+        newSceneKey = choice["scene_key"]
+        if(newSceneKey in dead_ends):
+            print("Welp, time to find ChatGPT")
+            print("--------------------------------")
+            input("Press ENTER to exit the program")
+        else:
+            newSceneDescription = story_data["scenes"][newSceneKey]["text"]
+            newSceneChoices = story_data["scenes"][newSceneKey]["choices"]
+            newScenePrint(newSceneDescription, newSceneChoices)
+    else:
+        while(validity == False):
+           userChoice = int(input("Please input a valid number."))
+           validity = checkValidChoice(userChoice, choiceCounter)
+
+
+#Start with the "start" scene
+newScenePrint(story_data["scenes"]["start"]["text"], story_data["scenes"]["choices"]["text"])
